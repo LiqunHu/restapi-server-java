61204934ea78253cf05933132e00539f0358a404
胡力群 <huliquns@126.com>
2022-05-07 14:33:19 +0800

    增加Junit test

--- src/test/java/com/example/services/api/test/TestControllerTest.java
+++ src/test/java/com/example/services/api/test/TestControllerTest.java
@@ -1,42 +1,44 @@
-package com.example.services.api;
+package com.example.services.api.test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.example.services.api.test.controller.TestController;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-public class ApplicationTest {
+class TestControllerTest {
 	
-	private MockMvc mvc;
+//	private MockMvc mvc;
 
-    @Before
-    public void setUp() {
-        mvc = MockMvcBuilders.standaloneSetup(new TestController()).build();
-    }
+//    @Before
+//    public void setUp() {
+//        mvc = MockMvcBuilders.standaloneSetup(new TestController()).build();
+//    }
 
 	@Test
 	public void testTestController() throws Exception {
 		// 测试TestController
 		RequestBuilder request;
 
+		MockMvc mvc = MockMvcBuilders.standaloneSetup(new TestController()).build();
+		
 		// 1、get查一下user列表，应该为空
 		request = post("/test/echo").contentType(MediaType.APPLICATION_JSON)
 				.content("{\"msg\":\"hello\"}");
 		mvc.perform(request).andExpect(status().isOk());
 	}
+
 }
